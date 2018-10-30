75ac8e9cbd50bb65c2bbf9ddd693ee1849150ae4
Jason Coon <jasoncoon@hotmail.com>
2018-10-30 09:36:07 -0500

    Added patterns, low power mode, etc.

--- DemoReel100-Buttons.ino
+++ DemoReel100-Buttons.ino
@@ -1,5 +1,7 @@
-#include <FastLED.h>
-#include <Button.h>
+#include <LowPower.h> // https://github.com/rocketscream/Low-Power
+#include <FastLED.h>  // https://github.com/FastLED/FastLED
+#include <Button.h>   // https://github.com/madleech/Button
+
 #include "Palettes.h"
 
 FASTLED_USING_NAMESPACE
@@ -21,25 +23,46 @@ FASTLED_USING_NAMESPACE
 //#define CLK_PIN     2
 //#define LED_TYPE    APA102
 #define LED_TYPE    WS2812
-#define COLOR_ORDER RGB
+#define COLOR_ORDER GRB
 #define NUM_LEDS    60
 CRGB leds[NUM_LEDS];
 
 #define FRAMES_PER_SECOND  120
 
-const uint8_t PIN_BUTTON_PATTERN = 12;
-const uint8_t PIN_BUTTON_BRIGHTNESS = 11;
+const uint8_t PIN_BUTTON_PATTERN = 3;
+const uint8_t PIN_BUTTON_BRIGHTNESS = 2;
 const uint8_t PIN_LED_STATUS = 13;
 
-uint8_t brightnesses[] = { 16, 32, 64, 128, 255 };
-uint8_t currentBrightnessIndex = 4;
+uint8_t brightnesses[] = { 255, 128, 64, 0, };
+uint8_t currentBrightnessIndex = 0;
+
+uint8_t currentPatternIndex = 0; // Index number of which pattern is current
+uint8_t hue = 0; // rotating "base color" used by many of the patterns
+uint8_t hueFast = 0; // faster rotating "base color" used by many of the patterns
+
+uint8_t autoplay = 0;
+uint8_t autoplayDuration = 10;
+unsigned long autoPlayTimeout = 0;
+
+uint8_t cyclePalettes = 1;
+uint8_t paletteDuration = 5;
+uint8_t currentPaletteIndex = 0;
+unsigned long paletteTimeout = 0;
 
 bool ledStatus = false;
 
-Button buttonBrightness(11);
-Button buttonPattern(12);
+Button buttonBrightness(2);
+Button buttonPattern(3);
+
+void wakeUp()
+{
+  Serial.println("Waking up...");
+}
 
 void setup() {
+  Serial.begin(115200);
+  Serial.println("setup");
+  
   // delay(3000); // 3 second delay for recovery
 
   FastLED.setMaxPowerInVoltsAndMilliamps(5, 500);
@@ -62,10 +85,24 @@ void setup() {
 
 // List of patterns to cycle through.  Each is defined as a separate function below.
 typedef void (*SimplePatternList[])();
-SimplePatternList patterns = { pride, colorWaves, rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
-
-uint8_t currentPatternIndex = 0; // Index number of which pattern is current
-uint8_t hue = 0; // rotating "base color" used by many of the patterns
+SimplePatternList patterns = {
+  pride,
+  colorWaves,
+  chaseRainbow,
+  chaseRainbow2,
+  chaseRainbow3,
+  chasePalette,
+  chasePalette2,
+  chasePalette3,
+  solidPalette,
+  solidRainbow,
+  rainbow,
+  rainbowWithGlitter,
+  confetti,
+  sinelon,
+  juggle,
+  bpm
+};
 
 void loop()
 {
@@ -85,7 +122,45 @@ void loop()
     nblendPaletteTowardPalette(currentPalette, targetPalette, 8);
   }
 
+  EVERY_N_MILLISECONDS( 4 ) {
+    hueFast++;  // slowly cycle the "base color" through the rainbow
+  }
+
+  if (autoplay == 1 && (millis() > autoPlayTimeout)) {
+    nextPattern();
+    autoPlayTimeout = millis() + (autoplayDuration * 1000);
+  }
+
+  if (cyclePalettes == 1 && (millis() > paletteTimeout)) {
+    nextPalette();
+    paletteTimeout = millis() + (paletteDuration * 1000);
+  }
+
   handleInput();
+
+  if (brightnesses[currentBrightnessIndex] == 0) {  
+    Serial.println("Going down...");
+    Serial.flush();
+  
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+    FastLED.show();
+    
+    // Allow wake up pin to trigger interrupt on low.
+    attachInterrupt(0, wakeUp, LOW);
+    attachInterrupt(1, wakeUp, LOW);
+    
+    // Enter power down state with ADC and BOD module disabled.
+    // Wake up when wake up pin is low.
+    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
+    
+    // Disable external pin interrupt on wake up pin.
+    detachInterrupt(0);
+    detachInterrupt(1);
+  
+    Serial.println("I have awoken!");
+    
+    nextBrightness();
+  }
 }
 
 void handleInput() {
@@ -111,17 +186,49 @@ void handleInput() {
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 
+// returns an 8-bit value that
+// rises and falls in a sawtooth wave, 'BPM' times per minute,
+// between the values of 'low' and 'high'.
+//
+//       /|      /|
+//     /  |    /  |
+//   /    |  /    |
+// /      |/      |
+//
+uint8_t beatsaw8( accum88 beats_per_minute, uint8_t lowest = 0, uint8_t highest = 255,
+                  uint32_t timebase = 0, uint8_t phase_offset = 0)
+{
+  uint8_t beat = beat8( beats_per_minute, timebase);
+  uint8_t beatsaw = beat + phase_offset;
+  uint8_t rangewidth = highest - lowest;
+  uint8_t scaledbeat = scale8( beatsaw, rangewidth);
+  uint8_t result = lowest + scaledbeat;
+  return result;
+}
+
 void nextBrightness()
 {
   // add one to the current brightness number, and wrap around at the end
   currentBrightnessIndex = (currentBrightnessIndex + 1) % ARRAY_SIZE(brightnesses);
   FastLED.setBrightness(brightnesses[currentBrightnessIndex]);
+  Serial.print("brightness: ");
+  Serial.println(brightnesses[currentBrightnessIndex]);
 }
 
 void nextPattern()
 {
   // add one to the current pattern number, and wrap around at the end
   currentPatternIndex = (currentPatternIndex + 1) % ARRAY_SIZE(patterns);
+  Serial.print("pattern: ");
+  Serial.println(currentPatternIndex);
+}
+
+void nextPalette()
+{
+  // add one to the current palette number, and wrap around at the end
+  currentPaletteIndex = (currentPaletteIndex + 1) % paletteCount;
+  Serial.print("palette: ");
+  Serial.println(currentPaletteIndex);
 }
 
 void rainbow()
@@ -152,19 +259,30 @@ void confetti()
   leds[pos] += CHSV( hue + random8(64), 200, 255);
 }
 
+// Updated sinelon (no visual gaps) by Mark Kriegsman
+// https://gist.github.com/kriegsman/261beecba85519f4f934
 void sinelon()
 {
-  // a colored dot sweeping back and forth, with fading trails
+  // a colored dot sweeping
+  // back and forth, with
+  // fading trails
   fadeToBlackBy( leds, NUM_LEDS, 20);
-  int pos = beatsin16(13, 0, NUM_LEDS);
-  leds[pos] += CHSV( hue, 255, 192);
+  CHSV color = CHSV(hue, 220, 255);
+  int pos = beatsin16(120, 0, NUM_LEDS - 1);
+  static int prevpos = 0;
+  if ( pos < prevpos ) {
+    fill_solid(leds + pos, (prevpos - pos) + 1, color);
+  } else {
+    fill_solid(leds + prevpos, (pos - prevpos) + 1, color);
+  }
+  prevpos = pos;
 }
 
 void bpm()
 {
   // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
   uint8_t BeatsPerMinute = 62;
-  CRGBPalette16 palette = PartyColors_p;
+  CRGBPalette16 palette = RainbowColors_p;
   uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
   for ( int i = 0; i < NUM_LEDS; i++) { //9948
     leds[i] = ColorFromPalette(palette, hue + (i * 2), beat - hue + (i * 10));
@@ -172,13 +290,96 @@ void bpm()
 }
 
 void juggle() {
-  // eight colored dots, weaving in and out of sync with each other
+  const uint8_t dotCount = 3;
+  uint8_t step = 192 / dotCount;
+  // colored dots, weaving in and out of sync with each other
   fadeToBlackBy( leds, NUM_LEDS, 20);
   byte dothue = 0;
-  for ( int i = 0; i < 8; i++) {
-    leds[beatsin16(i + 7, 0, NUM_LEDS)] |= CHSV(dothue, 200, 255);
-    dothue += 32;
+  for (int i = 0; i < dotCount; i++) {
+    CRGB color = CHSV(dothue, 255, 255);
+    if (dotCount == 3) {
+      if (i == 1) {
+        color = CRGB::Green;
+      }
+      else if (i == 2) {
+        color = CRGB::Blue;
+      }
+    }
+    leds[beatsin16(i + 15, 0, NUM_LEDS)] += color;
+    dothue += step;
   }
+}
+
+void solidRainbow()
+{
+  fill_solid(leds, NUM_LEDS, CHSV(hue, 255, 255));
+}
+
+void solidPalette()
+{
+  fill_solid(leds, NUM_LEDS, ColorFromPalette(currentPalette, hue));
+}
+
+void chaseRainbow() {
+  static uint8_t h = 0;
+
+  for (uint16_t i = NUM_LEDS - 1; i > 0; i--) {
+    leds[i] = leds[i - 1];
+  }
+  leds[0] = ColorFromPalette(RainbowStripeColors_p, h++);
+}
+
+void chaseRainbow2() {
+  const uint8_t step = 255 / NUM_LEDS;
+
+  for ( int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette(RainbowStripeColors_p, hueFast + i * step);
+  }
+}
+
+void chaseRainbow3()
+{
+  fadeToBlackBy(leds, NUM_LEDS, 20);
+  CHSV color = CHSV(hue, 220, 255);
+  int pos = beatsaw8(120, 0, NUM_LEDS - 1);
+  static int prevpos = 0;
+  if ( pos < prevpos ) {
+    fill_solid(leds + prevpos, NUM_LEDS - prevpos, color);
+    fill_solid(0, pos + 1, color);
+  } else {
+    fill_solid(leds + prevpos, (pos - prevpos) + 1, color);
+  }
+  prevpos = pos;
+}
+
+void chasePalette() {
+  for (uint16_t i = NUM_LEDS - 1; i > 0; i--) {
+    leds[i] = leds[i - 1];
+  }
+  leds[0] = ColorFromPalette(currentPalette, hue);
+}
+
+void chasePalette2() {
+  const uint8_t step = 255 / NUM_LEDS;
+
+  for ( int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette(currentPalette, hueFast + i * step);
+  }
+}
+
+void chasePalette3()
+{
+  fadeToBlackBy(leds, NUM_LEDS, 20);
+  CRGB color = ColorFromPalette(currentPalette, hue);
+  int pos = beatsaw8(120, 0, NUM_LEDS - 1);
+  static int prevpos = 0;
+  if ( pos < prevpos ) {
+    fill_solid(leds + prevpos, NUM_LEDS - prevpos, color);
+    fill_solid(0, pos + 1, color);
+  } else {
+    fill_solid(leds + prevpos, (pos - prevpos) + 1, color);
+  }
+  prevpos = pos;
 }
 
 // Pride2015 by Mark Kriegsman: https://gist.github.com/kriegsman/964de772d64c502760e5
